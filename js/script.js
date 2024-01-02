d9b34698077eabfcf39a3e27193edb2ecccaeab8
youngicthub <youngicthub@gmail.com>
2024-01-02 09:00:41 -0800

    Updated Nav

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', function () {
+        menu.classList.toggle('show');
+    });
+});
+
+
+  
